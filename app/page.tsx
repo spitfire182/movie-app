9c611467f71461ec2f5715458bef1b4e31d2edcb
Piotr <piotr.podg@gmail.com>
2025-10-16 21:37:16 +0200

    Initial commit - app structure

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default async function HomePage() {
+  // TODO: Fetch data on the server
+
+  return (
+    <main>
+      <h1>Movies</h1>
+      {/* MovieList component */}
+    </main>
+  );
+}
